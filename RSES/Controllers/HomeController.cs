ace46d160d0b9c1d81f558b2faab1cd34f09fe1d
Daniel <Ashton>
2013-01-14 08:38:42 +0000

    first commit

---
+++ RSES/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSES.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Red Squirrel Electrical Services";
+            
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
